65b23599d0d7c1ebb343769113c8c74f79202ebb
kimgeon <kimgeon0802@gmail.com>
2026-02-06 21:53:00 +0900

    1번, 2번 과제 완료

---
+++ python_homework/8_3_system_info.py
@@ -0,0 +1,13 @@
+'''
+os와 sys 모듈을 사용하여 시스템 정보를 출력하고 파일 경로를 다루는 프로그램을 작성하세요.
+출력결과:
+현재 작업 디렉토리: /Users/username/python_practice
+Python 버전: 3.9.7 (default, Oct 13 2021, 06:44:56)
+운영체제: posix
+환경 변수 PATH 일부: /usr/local/bin:/usr/bin:/bin
+파일 경로 정보:
+- 디렉토리: /Users/username/documents
+- 파일명: report.txt
+- 확장자: .txt
+파일 존재 여부: False
+'''
